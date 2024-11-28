3266df62bd4f22773dbf86bef3055801fb3d73cd
Your Name <youremail@example.com>
2024-11-28 12:13:55 -0600

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    experimental: {
+      appDir: true, // Ensure this is enabled for Next.js projects using the `app` directory.
+    },
+  };
+  
